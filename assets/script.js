aedc4e019c085fa23f96999b199dfef9cc9444f0
Servant Clément <clementServant@protonmail.com>
2023-10-17 11:20:08 +0200

    Ajout des fleche gauche et droite avec action au click

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,40 @@
-const slides = [
+/****** 
+ * Ajout des variables pour la réalisation du carrousel.
+*/
+
+// Récuperation de la fléche de gauche avec querySelector =>  dse la class= ".arrow-left"
+let arrowLeft = document.querySelector(".arrow_left")
+console.log(arrowLeft)
+
+// Ajout de l'eventListerner action au click sur l'icone de gauche
+arrowLeft.addEventListener("click", () => {
+	console.log("J'ai clické sur la fléche de gauche")
+})
+
+// Récuperation de la fléche de droite avec querySelector =>  dse la class= ".arrow-right"
+let arrowRight = document.querySelector(".arrow_right")
+console.log(arrowRight)
+
+// Ajout de l'eventListerner action au click sur l'icone de droite
+arrowRight.addEventListener("click", () => {
+	console.log("J'ai clické sur la fléche de droite")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const slides = [
 	{
 		"image":"slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
@@ -16,3 +52,4 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+*/
