1feb54059c160a105e06e2f061fe054239fd64f4
Servant Clément <clementServant@protonmail.com>
2023-10-24 15:21:22 +0200

    Fin de projet

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,69 @@
- /*******************
-* Ajout des variables pour la réalisation du carrousel.
+ /**
+* *Ajout des variables pour la réalisation du carrousel.
 */ 
 
 
-// Récuperation de la fléche de gauche avec querySelector =>  dse la class= ".arrow-left"
-let arrowLeft = document.querySelector(".arrow_left")
+// !Récuperation de la fléche de gauche avec querySelector 
+
+const arrowLeft = document.querySelector(".arrow_left")
 console.log(arrowLeft)
-
-// Ajout de l'eventListerner action au click sur l'icone de gauche
+// *Ajout de l'eventListerner action au click sur l'icone de gauche
 arrowLeft.addEventListener("click", () => {
-	console.log("J'ai clické sur la fléche de gauche")
+	const pointSelectionner = dot.findIndex(dot => dot.classList.contains("dot_selected"))
+	// Calculer l'index du point suivant
+	let pointPrecedent = pointSelectionner - 1 
+	// Vérifier si nous avons atteint la fin du tableau et revenir au premier point si nécessaire
+	if (pointPrecedent < 0) {
+		pointPrecedent = dot.length - 1
+	}
+	// Désélectionnez le point actuellement sélectionné
+	dot[pointSelectionner].classList.remove("dot_selected")
+	// sélectionnez le point actuellement sélectionné
+	dot[pointPrecedent].classList.add("dot_selected")
+	// Recuperation de limage du slider
+	const imageDuSlider = document.querySelector(".banner-img")
+	//changer l'image du slider
+    imageDuSlider.src = "./assets/images/slideshow/" + slides[pointPrecedent].image
+    // Passez à la diapositive suivante en utilisant nextIndex
+    const texteDuSlider = document.querySelector(".banner-tagLine")
+	 // Mettez à jour l'image et la ligne de balisage du slider
+    texteDuSlider.innerHTML = slides[pointPrecedent].tagLine
+	
+	console.log(pointPrecedent)
 })
 
-// Récuperation de la fléche de droite avec querySelector =>  dse la class= ".arrow-right"
-let arrowRight = document.querySelector(".arrow_right")
+// !Récuperation de la fléche de droite avec querySelector 
+
+const arrowRight = document.querySelector(".arrow_right")
 console.log(arrowRight)
-
-// Ajout de l'eventListerner action au click sur l'icone de droite
+// *Ajout de l'eventListerner action au click sur l'icone de droite
 arrowRight.addEventListener("click", () => {
-	console.log("J'ai clické sur la fléche de droite")
+	const pointSelectionner = dot.findIndex(dot => dot.classList.contains("dot_selected"))
+
+	let pointSuivant = pointSelectionner + 1 
+		
+	if (pointSuivant === dot.length) {
+		pointSuivant = 0
+	}
+
+	dot[pointSelectionner].classList.remove("dot_selected")
+	dot[pointSuivant].classList.add("dot_selected")	
+
+	const imageDuSlider = document.querySelector(".banner-img")
+    imageDuSlider.src = "./assets/images/slideshow/" + slides[pointSuivant].image
+    
+    const texteDuSlider = document.querySelector(".banner-tagLine")
+    texteDuSlider.innerHTML = slides[pointSuivant].tagLine
+	
+	console.log(pointSuivant)
 })
 
 
-/*********** Ajout des bullet points sur la partie basse du slider
+/** 
  * 
- * Ajout des bullets points depuis le script.js
+ * *Ajout des bullet points sur la partie basse du slider
+ * 
+ * ?Ajout des bullets points depuis le script.js
  * 
  */
 
@@ -35,28 +74,38 @@ for (let i = 0; i < 4; i++) {
 	let span = document.createElement("span")
 	span.classList.add("dot")
 	slider.appendChild(span)
+	// !Ajout du point d'affichage en cours.
 	dot.push(span)
 	dot[0].classList.add("dot_selected")
 }
-
-
 console.log(slider)
 
-/*const slides = [
+/**
+ * *Modification du slide au clic sur les fléches 
+ * 
+ */
+
+const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		image: "slide1.jpg",
+		tagLine: "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		image: "slide2.jpg",
+		tagLine: "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		image: "slide3.jpg",
+		tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		image: "slide4.png",
+		tagLine: "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]*/
+]
+/**
+ * *Ajout de la funtion d'affichage
+ */
+
+
+
